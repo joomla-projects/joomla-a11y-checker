89a3b3aac26e3b8222427bd90dc3b6bd5f399897
Fedik <getthesite@gmail.com>
2021-11-06 17:24:41 +0200

    fix use of sprintf

--- src/js/lang/en.js
+++ src/js/lang/en.js
@@ -57,7 +57,7 @@ export default {
     "notVisibleAlert": "The item you are trying to view is not visible; it may be hidden or inside of an accordion or tab component. Here\"s a preview: ",
 
     // Headings
-    "JOOMLA_A11Y_CHECKER_HEADING_EMPTY": "Empty heading found! To fix, delete this line or change its format from <span class=\"sa11y-red-text sa11y-bold\">Heading $(level)</span> to <span class=\"sa11y-bold\">Normal</span> or <span class=\"sa11y-bold\">Paragraph</span>.",
+    "JOOMLA_A11Y_CHECKER_HEADING_EMPTY": "Empty heading found! To fix, delete this line or change its format from <span class=\"sa11y-red-text sa11y-bold\">Heading %(level)</span> to <span class=\"sa11y-bold\">Normal</span> or <span class=\"sa11y-bold\">Paragraph</span>.",
     "JOOMLA_A11Y_CHECKER_HEADING_EMPTY_WITH_IMAGE": "Heading has no text, but contains an image. If this is not a heading, change its format from <span class=\"sa11y-red-text sa11y-bold\">Heading %(level)</span> to <span class=\"sa11y-bold\">Normal</span> or <span class=\"sa11y-bold\">Paragraph</span>. Otherwise, please add alt text to the image if it is not decorative.",
     "JOOMLA_A11Y_CHECKER_HEADING_FIRST": "The first heading on a page should usually be a Heading 1 or Heading 2. Heading 1 should be the start of the main content section, and is the main heading that describes the overall purpose of the page. Learn more about <a href=\"https://www.w3.org/WAI/tutorials/page-structure/headings/\" target=\"_blank\">Heading Structure. %(sa11yNewTab)</a>",
     "JOOMLA_A11Y_CHECKER_HEADING_LONG": "Heading is long! Headings should be used to organize content and convey structure. They should be brief, informative, and unique. Please keep headings less than 160 characters (no more than a sentence).",
@@ -73,7 +73,7 @@ export default {
     "JOOMLA_A11Y_CHECKER_LINK_EMPTY_LINK_NO_LABEL": "Link does not have discernable text that is visible to screen readers and other assistive technology. To fix:<ul><li>Add some concise text that describes where the link takes you.</li><li>If it is an <a href=\"https://a11y-101.com/development/icons-and-links\" target=\"_blank\">icon link or SVG,%(sa11yNewTab)</a> it is likely missing a descriptive label.</li><li>If you think this link is an error due to a copy/paste bug, consider deleting it.</li></ul>",
     "JOOMLA_A11Y_CHECKER_LINK_LABEL": "The descriptive label for this link is: <span class=\"sa11y-bold\">%(linkText)</span>",
     "JOOMLA_A11Y_CHECKER_LINK_STOPWORD": "Link text may not be descriptive enough out of context: <span class=\"sa11y-red-text sa11y-bold\">%(error)</span>",
-    "JOOMLA_A11Y_CHECKER_LINK_STOPWORD_TIP": "<span class=\"sa11y-bold\">Tip!</span> Link text should always be clear, unique, and meaningful. Avoid common words like &quot;click here&quot; or &quot;learn more&quot;.",
+    "JOOMLA_A11Y_CHECKER_LINK_STOPWORD_TIP": "<span class=\"sa11y-bold\">Tip!</span> Link text should always be clear, unique, and meaningful. Avoid common words like \"click here\"; or \"learn more\".",
     "JOOMLA_A11Y_CHECKER_LINK_URL": "Longer, less intelligible URLs used as link text might be difficult to listen to with assistive technology. In most cases, it is better to use human-readable text instead of the URL. Short URLs (such as a site's homepage) are okay.",
     "JOOMLA_A11Y_CHECKER_LINK_URL_TIP": "<span class=\"sa11y-bold\">Tip!</span> Link text should always be clear, unique, and meaningful so it could be understood out of context.",
 
