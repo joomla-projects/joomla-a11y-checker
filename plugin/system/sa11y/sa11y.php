0b85666f8e18f8e01ce29f9303626f7835fbcf34
brian teeman <brian@teeman.net>
2021-11-23 22:00:42 +0000

    Plugin Scaffolding

---
+++ plugin/system/sa11y/sa11y.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.sa11y
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Application\CMSApplicationInterface;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+/**
+ * Sa11y plugin to add an accessibility checker
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class PlgSystemSa11y extends CMSPlugin
+{
+	/**
+	 * Application object.
+	 *
+	 * @var    CMSApplicationInterface
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $app;
+
+  	/**
+	 * Method to check if the current user is allowed to see the debug information or not.
+	 *
+	 * @return  boolean  True if access is allowed.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	private function isAuthorisedDisplayChecker(): bool
+	{
+		static $result = null;
+
+		if ($result !== null)
+		{
+			return $result;
+		}
+
+		// If the user is not allowed to view the output then end here.
+		$filterGroups = (array) $this->params->get('filter_groups', []);
+
+		if (!empty($filterGroups))
+		{
+			$userGroups = $this->app->getIdentity()->get('groups');
+
+			if (!array_intersect($filterGroups, $userGroups))
+			{
+				$result = false;
+
+				return false;
+			}
+		}
+
+		$result = true;
+
+		return true;
+	}
+
+	/**
+	 * Add the checker.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onAfterDispatch()
+	{
+		// Only on site application
+		if (!$this->app->isClient('site'))
+		{
+			return false;
+		}
+
+	// Only on HTML documents
+		if ($document->getType() !== 'html')
+		{
+			return false;
+		}
+
+		// Are we in a modal?
+		if ($this->app->input->get('tmpl', '', 'cmd') === 'component')
+		{
+			return true;
+		}
+
+	// Load the checker if authorised
+		if (!$this->isAuthorisedDisplayChecker())
+		{
+			return false;
+		}
+
+		// Load language file.
+		$this->loadLanguage();
+
+		// Determine if it is an LTR or RTL language
+		$direction = Factory::getLanguage()->isRtl() ? 'right' : 'left';
+
+		// Detect the current active language
+		$lang = Factory::getLanguage()->getTag();
+
+		// Add plugin settings from the xml
+		$document->addScriptOptions(
+			'defaultOptions',
+			[
+				// Language
+				'langCode' => $this->params->get('langCode'),
+				'readabilityLang' => $this->params->get('readabilityLang'),
+				// Advanced Fieldset
+				'checkRoot' => $this->params->get('checkRoot'),
+				'readabilityRoot' => $this->params->get('readabilityRoot'),
+				'containerIgnore' => $this->params->get('containerIgnore'),
+				'outlineIgnore' => $this->params->get('outlineIgnore'),
+				'headerIgnore' => $this->params->get('headerIgnore'),
+				'imageIgnore' => $this->params->get('imageIgnore'),
+				'linkIgnore' => $this->params->get('linkIgnore'),
+				'linkIgnoreSpan' => $this->params->get('linkIgnore'),
+				'linksToFlag' => $this->params->get('linkIgnore'),
+				// Start up preferences
+				'contrast' => $this->params->get('contrast'),
+				'labels' => $this->params->get('labels'),
+				'links-advanced' => $this->params->get('links-advanced'),
+				'readability' => $this->params->get('readability'),
+			]
+		);
+
+		/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+		$wa = $document->getWebAssetManager();
+		$wa->useScript('sa11y');
+	}
+}
