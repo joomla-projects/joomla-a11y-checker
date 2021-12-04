f163b67583132e22540b12068027bdd79c9d7c68
dgrammatiko <d.grammatiko@gmail.com>
2021-12-04 14:45:08 +0100

    Run on load, on on DOM ready

--- docs/pages/script.js
+++ docs/pages/script.js
@@ -1,10 +1,10 @@
 import { Jooa11y, Lang } from '../assets/js/joomla-a11y-checker.esm.js';
 import EngSetting from '../assets/js/lang/en.js';
 
-// Set translations
-Lang.addI18n(EngSetting.strings);
-
-// Instantiate
-const checker = new Jooa11y(EngSetting.options);
-
-checker.doInitialCheck();
+window.addEventListener('load', () => {
+  // Set translations
+  Lang.addI18n(EngSetting.strings);
+  // Instantiate
+  const checker = new Jooa11y(EngSetting.options);
+  checker.doInitialCheck();
+});
